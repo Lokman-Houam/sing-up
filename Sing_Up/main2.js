8ef0207689cb03abe64940ef77bd5708dd7c7ea8
Lokman-Houam <121229914+Lokman-Houam@users.noreply.github.com>
2024-02-16 18:55:08 +0100

    Add files via upload

---
+++ Sing_Up/main2.js
@@ -0,0 +1,5 @@
+var myElement = document.getElementById("ll");
+setTimeout(function () {
+  //myElement.style.backgroundImage = "linear-gradient(black 50%, black 100%)";
+  myElement.style.backgroundColor = "#181818";
+}, 4600);
