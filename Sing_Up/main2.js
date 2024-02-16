abaa14d45483d8ba5b411f54e6a9574ba11a2b2e
Lokman-Houam <121229914+Lokman-Houam@users.noreply.github.com>
2024-02-16 19:15:59 +0100

    Delete Sing_Up directory

--- Sing_Up/main2.js
+++
@@ -1,5 +0,0 @@
-var myElement = document.getElementById("ll");
-setTimeout(function () {
-  //myElement.style.backgroundImage = "linear-gradient(black 50%, black 100%)";
-  myElement.style.backgroundColor = "#181818";
-}, 4600);
